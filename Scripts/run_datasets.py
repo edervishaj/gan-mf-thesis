85fb407613cadf871747f68e978b97ab8564290e
Ervin Dervishaj <vindervishaj@gmail.com>
2020-02-12 18:29:58 +0100

    Script that generates all datasets' stats and distributions

--- Scripts/run_datasets.py
+++ Scripts/run_datasets.py
@@ -6,24 +6,35 @@
 @email: vindervishaj@gmail.com
 '''
 
+import os
+
 from datasets.BookCrossing import BookCrossing
 from datasets.CiaoDVD import CiaoDVD
 from datasets.Delicious import Delicious
+from datasets.Jester import Jester
 from datasets.LastFM import LastFM
 from datasets.Movielens import Movielens
+from datasets.Movies import Movies
 
 if __name__ == "__main__":
-    reader = BookCrossing(use_local=True, force_rebuild=True, implicit=True, save_local=True, verbose=True,
-                     min_ratings=1, remove_top_pop=0.0, header=True)
-    URM = reader.get_URM_full()
-    URM_train = reader.get_URM_train()
-    URM_test = reader.get_URM_test()
-    URM_valid = reader.get_URM_validation()
-    # print(URM_train.nnz)
-    # print(URM_test.nnz)
-    # print(URM_valid.nnz)
-    print(URM.shape)
-    print(URM_train.shape)
-    print(URM_test.shape)
-    print(URM_valid.shape)
-    reader.describe()
+    # Generic parameters for each dataset
+    kwargs = {}
+    kwargs['use_local'] = True
+    kwargs['force_rebuild'] = False
+    kwargs['implicit'] = False
+    kwargs['save_local'] = True
+    kwargs['verbose'] = False
+    kwargs['split'] = False
+
+    datasets = [BookCrossing, CiaoDVD, Delicious, Jester, LastFM, '1M', '10M', 'latest']
+    # datasets = [BookCrossing]
+
+    for d in datasets:
+        if isinstance(d, str):
+            reader = Movielens(version=d, **kwargs)
+        else:
+            reader = d(**kwargs)
+        path = os.path.join(reader.all_datasets_dir, 'stats')
+        if not os.path.exists(path):
+            os.makedirs(path)
+        reader.describe(save_plots=True, path=path)
