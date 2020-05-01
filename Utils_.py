8d845c11fa216bd1987aac8e1e8ae4ca1cdf7d2f
Ervin Dervishaj <ervin.dervishaj@contentwise.tv>
2020-05-01 22:23:14 +0200

    Added Gini Index

--- Utils_.py
+++ Utils_.py
@@ -244,3 +244,16 @@ def plot(feed, title, save_dir, xlabel='epochs', ylabel=None):
 
     save_path = os.path.join(save_dir, title + '.png')
     fig.savefig(save_path, bbox_inches="tight")
+
+def gini(array):
+    """ From https://github.com/oliviaguest/gini"""
+    # based on bottom eq: http://www.statsdirect.com/help/content/image/stat0206_wmf.gif
+    # from: http://www.statsdirect.com/help/default.htm#nonparametric_methods/gini.htm
+    array = array.flatten() #all values are treated equally, arrays must be 1d
+    if np.amin(array) < 0:
+        array -= np.amin(array) #values cannot be negative
+    array += 0.0000001 #values cannot be 0
+    array = np.sort(array) #values must be sorted
+    index = np.arange(1,array.shape[0]+1) #index per array element
+    n = array.shape[0]#number of array elements
+    return ((np.sum((2 * index - n  - 1) * array)) / (n * np.sum(array))) #Gini coefficient
